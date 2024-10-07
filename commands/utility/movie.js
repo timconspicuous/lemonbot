da9d6280541f04f29e9f0a8ce76cd7b6c56e075b
timconspicuous <bipolarfuchs@gmail.com>
2024-10-07 19:47:43 +0200

    OMDb API request + new movie embed

--- commands/utility/movie.js
+++ commands/utility/movie.js
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
-import { getDTDDInfo } from '../../utils/movieUtils.js';
+import { DDTDSearch, DDTDMedia, getTriggers, OMDbSearch } from '../../utils/movieUtils.js';
 
 export const data = new SlashCommandBuilder()
     .setName('movie')
@@ -13,26 +13,40 @@ export async function execute(interaction) {
     await interaction.deferReply();
     const title = interaction.options.getString('title');
 
-    const data = await getDTDDInfo(title);
+    const data = await DDTDSearch(title);
     const { id,
         name,
-        genre,
-        releaseYear,
         tmdbId,
         imdbId,
-        posterImage,
         overview,
     } = data.items[0];
+
+    let triggerString;
+    if (id) {
+        const triggerData = await DDTDMedia(id);
+        const { categories, triggerCount } = getTriggers(triggerData);
+        triggerString = `Found ${triggerCount} potentially triggering events in the categories ${categories.join(", ")}.`;
+    }
+    let omdbData;
+    if (imdbId) {
+        omdbData = await OMDbSearch(imdbId, null);
+    } else {
+        omdbData = await OMDbSearch(null, title);
+    }
 
     const embed = new EmbedBuilder()
         .setColor(0x0099FF)
         .setTitle(name)
         .addFields(
-            { name: 'Info', value: `${genre.charAt(0).toUpperCase() + genre.slice(1)}, ${releaseYear}`, inline: false },
+            { name: 'Year', value: omdbData.Year, inline: true },
+            { name: 'Runtime', value: omdbData.Runtime, inline: true },
+            { name: 'Genre', value: omdbData.Genre, inline: true },
+            //{ name: 'Director', value: omdbData.Director, inline: true },
             { name: 'Plot', value: overview, inline: false },
-            { name: 'Links', value: `[Letterboxd](https://www.letterboxd.com/tmdb/${tmdbId}) | [IMDb](https://www.imdb.com/title/${imdbId}) | [DoesTheDogDie](https://www.doesthedogdie.com/media/${id})`, inline: false }
+            { name: 'Links', value: `[Letterboxd](https://www.letterboxd.com/tmdb/${tmdbId}) | [IMDb](https://www.imdb.com/title/${omdbData.imdbID}) | [DoesTheDogDie](https://www.doesthedogdie.com/media/${id})`, inline: false },
+            { name: 'Triggers', value: triggerString, inline: false },
         )
-        .setThumbnail(`https://www.doesthedogdie.com/content/200/0/${posterImage}`)
+        .setThumbnail(omdbData.Poster)
         .setTimestamp();
 
     await interaction.editReply({ embeds: [embed] });
--- utils/movieUtils.js
+++ utils/movieUtils.js
@@ -3,8 +3,8 @@ import { load } from "jsr:@std/dotenv";
 
 await load({ export: true });
 
-// Does the Dog Die API
-export async function getDTDDInfo(movieTitle) {
+// Does the Dog Die
+export async function DDTDSearch(movieTitle) {
     const queryParam = movieTitle.toLowerCase();
     try {
         const response = await axios.get('https://www.doesthedogdie.com/dddsearch', {
@@ -16,10 +16,59 @@ export async function getDTDDInfo(movieTitle) {
                 q: queryParam,
             }
         });
-        
+
         return response.data;
     } catch (error) {
-        console.error('Error getting Does the Dog Die info:', error.response.data);
+        console.error('Error getting Does the Dog Die data:', error.response.data);
+        throw error;
+    }
+}
+
+export async function DDTDMedia(itemId) {
+    try {
+        const response = await axios.get(`https://www.doesthedogdie.com/media/${itemId}`, {
+            headers: {
+                'Accept': 'application/json',
+                'X-API-KEY': Deno.env.get("DTDD_API_KEY"),
+            }
+        });
+
+        return response.data;
+    } catch (error) {
+        console.error('Error getting Does the Dog Die data:', error.response.data);
+        throw error;
+    }
+}
+
+export function getTriggers(data) {
+    const uniqueCategories = new Set();
+    let triggerCount = 0;
+
+    // Iterate over topicItemStats
+    data.topicItemStats.forEach((item) => {
+        if (item.yesSum > item.noSum) {
+            uniqueCategories.add(item.topic.TopicCategory.name);
+            triggerCount++;
+        }
+    });
+
+    return { categories: Array.from(uniqueCategories), triggerCount };
+}
+
+// Open Movie Database
+export async function OMDbSearch(imdbId = null, title = null) {
+    try {
+        const response = await axios.get(`https://www.omdbapi.com/`, {
+            params: {
+                apikey: Deno.env.get("OMDB_API_KEY"),
+                i: imdbId,
+                t: title,
+            }
+        })
+
+        return response.data;
+    } catch (error) {
+        console.error('Error getting Open Movie Database data:', error.response.data);
         throw error;
     }
 }
